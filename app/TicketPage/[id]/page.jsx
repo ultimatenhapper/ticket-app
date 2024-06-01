2c90244b3bcff8b1d7ea80c3751041701f9781e7
Javi <belikemike3057@hotmail.com>
2024-06-01 11:40:51 +0200

    Implementing a CRUD with tickets

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,32 @@
+import EditTicketForm from "@/app/(components)/EditTicketForm";
 import React from "react";
 
-const TicketPage = ({ params }) => {
-  return <div>TicketPage ${params.id}</div>;
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to get ticket.");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+const TicketPage = async ({ params }) => {
+  const EDIT_MODE = params.id === "new" ? false : true;
+  let updateTicketData = {};
+
+  if (EDIT_MODE) {
+    updateTicketData = await getTicketById(params.id);
+    updateTicketData = updateTicketData.foundTicket;
+  } else {
+    updateTicketData = {
+      _id: "new",
+    };
+  }
+  return <EditTicketForm ticket={updateTicketData} />;
 };
 
 export default TicketPage;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import TicketCard from "./(components)/TicketCard";
+import _ from "lodash";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/tickets", {
+      cache: "no-store",
+    });
+    const { tickets } = await res.json();
+    const sortedTickets = _.orderBy(
+      tickets,
+      [(ticket) => getStatusOrder(ticket.status), "priority", "progress"],
+      ["asc", "desc", "asc"]
+    );
+    return sortedTickets;
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+  }
+};
+
+const getStatusOrder = (status) => {
+  switch (status) {
+    case "started":
+      return 1;
+    case "not started":
+      return 2;
+    case "done":
+      return 3;
+    default:
+      return 4;
+  }
+};
+
+const Dashboard = async () => {
+  const tickets = await getTickets();
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+  return (
+    <div className="p-5">
+      {tickets &&
+        uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+          <div key={categoryIndex} className="mb-4">
+            <h2>{uniqueCategory}</h2>
+            <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+              {tickets
+                .filter((ticket) => ticket.category === uniqueCategory)
+                .map((filteredTicket, _index) => (
+                  <TicketCard
+                    id={_index}
+                    key={_index}
+                    ticket={filteredTicket}
+                  />
+                ))}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default Dashboard;
